98b12de485899fd821fae60039bad753f0c5486f
G060N0FF <georgi.hr.dim@gmail.com>
2021-02-24 00:54:07 +0200

    WS to WSS in routing

--- PicPost/app/routing.py
+++ PicPost/app/routing.py
@@ -4,5 +4,5 @@ from . import consumers
 
 # routing to the consumer
 websocket_urlpatterns=[
-    re_path(r'ws/chat/(?P<room_name>\w+)/$',consumers.ChatConsumer),
+    re_path(r'wss/chat/(?P<room_name>\w+)/$',consumers.ChatConsumer),
 ]
